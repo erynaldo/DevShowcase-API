27fe67f515e30fc273445d48118d457e4a391ec2
erynaldo <erynaldo@hotmail.com>
2026-09-25 20:47:35 -0300

    corrigindo erro2

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,5 @@ router.use('/profiles', require('./profile.routes'));
 router.use('/projects', require('./project.routes'));
 router.use('/technologies', require('./technology.routes'));
 router.use('/feedbacks', require('./feedback.routes'));
-// router.use('/projects/:id/feedbacks', require('./feedback.routes'));
+router.use('/projects/:id/feedbacks', require('./feedback.routes'));
 module.exports = router;
